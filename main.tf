106afb89e343cb1661766a4cf817934540f386f6
SDenn12 <110126036+SDenn12@users.noreply.github.com>
2022-09-09 14:01:21 +0100

    read me updated

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "app_instance" {
   instance_type = "t2.micro"
 
   # using which ami
-  ami = data.aws_ami.server_ami.id
+  ami = data.aws_ami.ubuntu.id
 
   # how to name your instance
   tags = {
@@ -29,3 +29,58 @@ resource "aws_instance" "app_instance" {
   }
 
 }
+
+resource "aws_instance" "database_instance" {
+
+  # instance type
+  instance_type = "t2.micro"
+
+  # using which ami
+  ami = data.aws_ami.ubuntu.id
+
+  # how to name your instance
+  tags = {
+    Name = "eng122_samuel_terraform_db"
+  }
+
+
+  # how to attach your pem key to the app instance
+  key_name = "eng122_sam_pem"
+
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  subnet_id = aws_subnet.pub_subnet.id
+
+  root_block_device {
+    volume_size = 8
+  }
+
+}
+
+resource "aws_instance" "controller_instance" {
+
+  # instance type
+  instance_type = "t2.micro"
+
+  # using which ami
+  ami = data.aws_ami.controller.id
+
+  # how to name your instance
+  tags = {
+    Name = "eng122_samuel_ansible_controller"
+  }
+
+
+  # how to attach your pem key to the app instance
+  key_name = "eng122_sam_pem"
+
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  subnet_id = aws_subnet.pub_subnet.id
+
+  root_block_device {
+    volume_size = 8
+  }
+
+}
+
